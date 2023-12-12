24d0c2b9bf4e9db6674d9e9806ea0185e96842c7
tommys0 <149568540+tommys0@users.noreply.github.com>
2023-12-12 16:48:56 +0100

    Added delete button, new design

--- script.js
+++ script.js
@@ -1,38 +1,81 @@
 let comments = [
     {
+        id: 1,
         username: "tommyasek",
-        date: "2023-01-03T03:02:00Z",
-        text: "velice pekne video"
+        date: "2023-02-10T08:30:00",
+        text: "velice pekne video",
+        profilePic: "https://placekitten.com/g/200/300"
     },
     {
+        id: 2,
         username: "700moradens007",
-        date: "2023-01-03T12:53:00Z",
-        text: "jako pokazde, uzasne video!"
+        date: "2023-05-12T15:45:00",
+        text: "jako pokazde, uzasne video!",
+        profilePic: "https://placekitten.com/g/200/300"
     },
     {
+        id: 3,
         username: "pademojo",
-        date: "2023-01-03T02:39:00Z",
-        text: "za me je bmw lepsi"
-    },
+        date: "2023-11-16T12:00:00",
+        text: "za me je bmw lepsi",
+        profilePic: "https://placekitten.com/g/200/300"
+    }
 ];
 
-let commentsSection = document.getElementById("comments-section");
+function showComments() {
+    const commentsSection = document.getElementById("comments-section");
+    commentsSection.innerHTML = "";
 
-comments.forEach(comment => {
-    let commentElement = document.createElement("div");
-    commentElement.classList.add("comment");
+    comments.forEach(comment => {
+        const visibleComment = createComment(comment);
+        commentsSection.appendChild(visibleComment);
+    });
+}
 
-    let userInfoElement = document.createElement("p");
-    userInfoElement.classList.add("user-info");
-    let commentDate = new Date(comment.date);
-    userInfoElement.textContent = `${comment.username} - ${commentDate.toLocaleDateString()} ${commentDate.toLocaleTimeString()}`;
+function createComment(comment) {
+    const theComment = document.createElement("div");
+    theComment.classList.add("comment");
 
-    let textElement = document.createElement("p");
-    textElement.classList.add("comment-text");
-    textElement.textContent = comment.text;
+    const profilePicture = new Image();
+    profilePicture.src = comment.profilePic;
+    profilePicture.alt = `${comment.username}'s profile picture`;
+    profilePicture.classList.add("profile-pic");
+    theComment.appendChild(profilePicture);
 
-    commentElement.appendChild(userInfoElement);
-    commentElement.appendChild(textElement);
+    const userInfo = document.createElement("div");
+    userInfo.classList.add("user-info");
 
-    commentsSection.appendChild(commentElement);
-});
+    const username = document.createElement("p");
+    username.classList.add("username");
+    username.textContent = comment.username;
+
+    const text = document.createElement("p");
+    text.classList.add("comment-text");
+    text.textContent = comment.text;
+
+    const deleteIcon = document.createElement("i");
+    deleteIcon.dataset.id = comment.id;
+    deleteIcon.classList.add("fas", "fa-trash-alt", "delete-icon");
+    deleteIcon.addEventListener("click", (e) => deleteComment(e));
+
+    userInfo.appendChild(username);
+    userInfo.appendChild(text);
+    theComment.appendChild(userInfo);
+    theComment.appendChild(deleteIcon);
+
+    const dateTime = document.createElement("p");
+    dateTime.classList.add("comment-time");
+    const commentDate = new Date(comment.date);
+    dateTime.textContent = `${commentDate.toLocaleDateString()} ${commentDate.toLocaleTimeString()}`;
+    theComment.appendChild(dateTime);
+
+    return theComment;
+}
+
+function deleteComment(event) {
+    const commentId = parseInt(event.target.dataset.id);
+    comments = comments.filter(comment => comment.id !== commentId);
+    showComments();
+}
+
+showComments();
